04d4f6e3e28bf8a87b45fb4fed6360b99edca968
Miles Gilbert <miles.gilbert@gmail.com>
2025-03-03 23:18:04 -0600

    playing around with popcorn

--- playground/popcorn/main.js
+++ playground/popcorn/main.js
@@ -2,12 +2,18 @@ let time = 0;
 let rotation = 0;
 let baseSize; // Base size for scaling
 let spawnCount = 37;
+let padding = 2.5;
+let maxParticles = 1000;
 
 // Matter.js variables
 let engine;
 let world;
 let particles = [];
 let boundaries = [];
+let previousColor;
+
+const emojis = ['❤️', '🧡', '💛', '💚', '💙', '💜', '🖤', '😂', '😒', '👀', '🥵', '👽', '🦶'];
+
 
 const reduceBtn = document.getElementById('reduceBtn');
 const particleCount = document.getElementById('particleCount');
@@ -68,6 +74,7 @@ function createBoundaries() {
 function setup() {
     const canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent('#canvasContainer');
+    previousColor = [random(0, 255), random(0, 255), random(0, 255)];
     baseSize = calculateBaseSize();
     setupPhysics();
 }
@@ -79,15 +86,21 @@ function windowResized() {
 }
 
 class PhysicsParticle {
-    constructor(x, y) {
+    constructor(x, y, color) {
         this.size = random(baseSize * 0.75, baseSize * 1.5);
-        this.body = Matter.Bodies.rectangle(x, y, this.size, this.size, {
-            friction: 0.3,
-            restitution: 0.4,
-            angle: random(TWO_PI),
-            density: 0.001
+        // this.body = Matter.Bodies.rectangle(x, y, this.size, this.size, {
+        //     friction: 0.3,
+        //     restitution: 0.4,
+        //     angle: random(TWO_PI),
+        //     density: 0.001
+        // });
+        // create a circular body
+        this.body = Matter.Bodies.circle(x, y, this.size/2 + padding, {
+           friction: 0.3,
+           restitution: 0.4,
+           angle: random(TWO_PI),
+           density: 0.001
         });
-        
         // Add some initial velocity
         Matter.Body.setVelocity(this.body, { 
             x: random(-5, 5),
@@ -96,6 +109,8 @@ class PhysicsParticle {
         
         Matter.World.add(world, this.body);
         this.alpha = 255;
+        this.color = color;
+        previousColor = this.color;
     }
 
     draw() {
@@ -107,8 +122,13 @@ class PhysicsParticle {
         rotate(angle);
         rectMode(CENTER);
         noStroke();
-        fill(255, this.alpha);
-        square(0, 0, this.size);
+        fill(this.color[0], this.color[1], this.color[2], this.alpha);
+        circle(0, 0, this.size);
+        // draw text
+        //fill(255);
+        // textAlign(CENTER, CENTER);
+        // textSize(baseSize);
+        // text(this.emoji, 0, 0);
         pop();
     }
 
@@ -130,21 +150,13 @@ function draw() {
         line(0, y, width, y);
     }
     
-    // Draw grid of animated squares
-    let gridSize = baseSize * 4;
-    for(let x = 0; x < width; x += gridSize) {
-        for(let y = 0; y < height; y += gridSize) {
-            drawConcentricSquares(x, y, gridSize);
-        }
-    }
-    
     // Draw all particles
     for (let particle of particles) {
         particle.draw();
     }
     
     // Clean up particles if too many
-    if (particles.length > 300) {
+    if (particles.length > maxParticles) {
         let oldParticle = particles.shift();
         oldParticle.remove();
     }
@@ -165,6 +177,13 @@ function draw() {
     
     time += 0.02;
     rotation += 0.01;
+    // Draw grid of animated squares
+    let gridSize = baseSize;
+    for(let x = 0; x < width; x += gridSize) {
+        for(let y = 0; y < height; y += gridSize) {
+            drawConcentricSquares(x, y, gridSize);
+        }
+    }
 }
 
 function drawConcentricSquares(x, y, size) {
@@ -230,8 +249,18 @@ function createParticles(x, y) {
     if (document.getElementById('reduceBtn').contains(event.target)) {
         return;
     }
+    // use noise to determine color based on rgb value of previousColor
+    console.log(previousColor);
+    let noiseValue1 = noise(previousColor[0]);
+    let noiseValue2 = noise(previousColor[1]);
+    let noiseValue3 = noise(previousColor[2]);
+    let r = map(noiseValue1, 0, 1, 0, 255);
+    let g = map(noiseValue2, 0, 1, 0, 255);
+    let b = map(noiseValue3, 0, 1, 0, 255);
+    let color = [r, g, b];
+
     for (let i = 0; i < spawnCount; i++) {
-        particles.push(new PhysicsParticle(x, y));
+        particles.push(new PhysicsParticle(x, y, color));
     }
 }
 
